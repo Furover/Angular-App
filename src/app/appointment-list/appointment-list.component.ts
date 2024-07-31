e89e17a11f3b216a724b6427d7b5e5691d61de0d
Furover <suspicious.porcupine379@passinbox.com>
2024-07-31 19:28:26 -0300

    Simple appointment list maker

---
+++ src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
+
+@Component({
+  selector: 'app-appointment-list',
+  templateUrl: './appointment-list.component.html',
+  styleUrl: './appointment-list.component.css'
+})
+export class AppointmentListComponent {
+
+  newAppointmentTitle: string = ""
+  newAppointmentDate: Date = new Date()
+  appointments: Appointment[] = []
+
+  addAppointment() {
+    if (this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
+      let newAppointment: Appointment = {
+        id: Math.floor(Math.random() * 10),
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate
+      }
+      this.appointments.push(newAppointment)
+      this.newAppointmentDate = new Date()
+      this.newAppointmentTitle = ""
+    }
+  }
+
+  deleteAppointment(index: number) {
+
+    this.appointments.splice(index, 1)
+
+  }
+
+
+}
