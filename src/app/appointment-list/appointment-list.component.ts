cffdb2968669ccde7b837408ccdfbebd85766e41
Furover <suspicious.porcupine379@passinbox.com>
2024-07-31 21:24:13 -0300

    styled and storaged appointments locally

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrl: './appointment-list.component.css'
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit {
 
+  
   newAppointmentTitle: string = ""
   newAppointmentDate: Date = new Date()
+
   appointments: Appointment[] = []
+  
+  ngOnInit(): void {
+    let savedAppointments = localStorage.getItem("appointments")
+
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : []
+  }
 
   addAppointment() {
     if (this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
@@ -22,12 +31,14 @@ export class AppointmentListComponent {
       this.appointments.push(newAppointment)
       this.newAppointmentDate = new Date()
       this.newAppointmentTitle = ""
+      localStorage.setItem("appointments", JSON.stringify(this.appointments))
     }
   }
 
   deleteAppointment(index: number) {
 
     this.appointments.splice(index, 1)
+    localStorage.setItem("appointments", JSON.stringify(this.appointments))
 
   }
 
